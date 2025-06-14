38c6de126dc05f23e3878e39742b5d05a919aa66
Sid <siddchow@icloud.com>
2025-06-13 20:26:43 -0400

    added burger for phones

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,3 +2,20 @@ import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 // ..
 AOS.init();
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const burgerContainer = document.getElementById('burger-container');
+    const burger = document.getElementById('burger');
+    const nav = document.getElementById('nav-container');
+  
+    if (burgerContainer && burger && nav) {
+      burgerContainer.addEventListener('click', () => {
+        console.log('Burger clicked!');
+        nav.classList.toggle('active');
+        burger.classList.toggle('active');
+      });
+    }
+  });
